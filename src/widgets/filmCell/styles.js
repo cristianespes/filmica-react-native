67f40b49877a0a03fcf3992262997553ddec6c3e
Cristian Espes <christian_zgza@hotmail.com>
2019-04-02 00:25:26 +0200

    Shadow and border added.

--- src/widgets/filmCell/styles.js
+++ src/widgets/filmCell/styles.js
@@ -4,14 +4,19 @@ import * as colors from '../../commons/colors';
 
 const styles = StyleSheet.create({
      cell: {
-        width: '50%',
-        height: width / 1.4,
-        padding: 4
+         width: '50%',
+         height: width / 1.4,
+         padding: 4,
+         shadowColor: colors.black,
+         shadowOffset: { width: 0, height: 2},
+         shadowOpacity: 0.8,
+         elevation: 5
      },
      image: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'cover'
+         borderRadius: 4,
+         width: '100%',
+         height: '100%',
+         resizeMode: 'cover'
      }
 });
 
